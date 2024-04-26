6d8261a54266af45039c123e65fa23d876c79dac
GGon77 <115796575+GGon77@users.noreply.github.com>
2024-04-26 08:15:50 -0300

    im horiedi :(

--- main.js
+++ main.js
@@ -21,5 +21,16 @@ function aumentaTamanho(){
     }
     numeroSenha.textContent = tamanhoSenha;
 }
+const campoSenha = document.querySelector('#campo-senha');
 
-console.log(botoes);
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+            let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+            numeroAleatorio = Math.floor(numeroAleatorio);
+            senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+    }
